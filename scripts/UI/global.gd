40977c3d7f20b4611364057aa2bbf0eadaea1ed8
XiaoLin-001 <evanpan001@gmail.com>
2025-06-14 17:21:49 +0800

    Fix main menu window mode black margin bug

--- scripts/UI/global.gd
+++ scripts/UI/global.gd
@@ -9,6 +9,8 @@ var multiplayer_ui_status: bool = true
 var multiplayer_IP: String = "" 
 var server_port: int = 1
 var master_volume: float = 0.5
+var resolution_index: int = 2
+var screen_mode_index: int = 0	
 
 func _ready():
 	AudioServer.set_bus_volume_db(0, linear_to_db(master_volume))
